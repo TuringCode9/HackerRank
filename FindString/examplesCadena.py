467dd18416cf9e4abc4158026c4e27872f26bdf8
zai <danielguillermoprogramming@gmail.com>
2022-05-04 23:25:04 -0500

    FindString

---
+++ FindString/examplesCadena.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Concatenando una cadena con una variable.
+cadena = "Hola mundo"
+print("Decimos "+ cadena)
+
+# Convirtiendo una cadena en minuscula.
+mensaje = "HOLA ESTAMOS APRENDIENDO A PROGRAMAR"
+print(mensaje.lower())
+
+# Convirtiendo una cadena en mayuscula.
+mensaje = "hola buenos días"
+print(mensaje.upper())
+
+
+
+
+
+
+
+
+
+
+
+
